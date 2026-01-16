5908be9918c0488d1a0320469452b31a10a7889a
francomarinozzi <francomarinozzi4@gmail.com>
2026-01-16 11:14:54 -0300

    pghstore

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config({quiet:true});
-
+const pg = require('pg');
 const isProduction = process.env.NODE_ENV === 'production';
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
